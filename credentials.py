ea28e037c7e83b7bb91b68702362125e39e26761
Azfrd <82478408+Azfrd08@users.noreply.github.com>
2024-04-04 22:56:41 +0800

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,1 +1,1 @@
-uri = "mongodb+srv://Azfrd:<password>@blog.f3naoay.mongodb.net/?retryWrites=true&w=majority&appName=Blog"
+uri = "mongodb+srv://Azfrd:YouAreDead69@blog.f3naoay.mongodb.net/?retryWrites=true&w=majority&appName=Blog"
